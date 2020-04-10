b047bc4132bca091a511a9d4869706cd845667e9
likunyao <nwpulky@gmail.com>
2020-04-10 19:59:17 +0800

    user table

--- models/models.go
+++ models/models.go
@@ -10,11 +10,10 @@ import (
 
 var db *gorm.DB
 
-type Model struct {
-	ID         int `gorm:"primary_key" json:"id"`
-	CreatedOn  int `json:"created_on"`
-	ModifiedOn int `json:"modified_on"`
-	DeletedOn  int `json:"deleted_on"`
+type User struct {
+	gorm.Model
+	Username string
+	Password string
 }
 
 func Setup() {
